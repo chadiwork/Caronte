b996e40032e25591c27ecad4516c36e3a10ee936
Vlady <vlady.yakovenko@live.it>
2016-01-28 16:11:28 +0100

    0.3.2

--- src/Finestra.java
+++ src/Finestra.java
@@ -1,6 +1,5 @@
 import adt.Auto;
 import adt.Traghetto;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,89 +12,110 @@ import java.awt.event.ActionListener;
  */
 public class Finestra extends JFrame {
 
-	private JPanel rootPanel;
-    private JTextField inputField;
-	private JPanel pnlStoriaViaggi;
-    private JButton btnCaricaAuto;
-    private JLabel lblConteggio;
-	private JPanel pnlCenter;
-	private JPanel pnlDX;
-	private JTextArea txtAreaViaggi;
-    private JPanel pnlTarga;
-    private JTextField inputFieldTarga;
-    private JTextField inputFieldLunghezza;
-    private JPanel pnlLunghezza;
-    private JPanel pnlRiempimentoTraghetto;
-    private JPanel pnlStoriaInserimenti;
-    private JLabel lblUltimoInserito;
-    private JButton btnEseguiViaggio;
-    private JTextArea txtAreaInseriti;
-	private JPanel pnlTasti;
-	private JLabel lblEseguite;
-	private boolean semaforo;
+private JPanel rootPanel;
+private JTextField inputField;
+private JPanel pnlStoriaViaggi;
+private JButton btnCaricaAuto;
+private JLabel lblConteggio;
+private JPanel pnlCenter;
+private JPanel pnlDX;
+private JTextArea txtAreaViaggi;
+private JPanel pnlTarga;
+private JTextField inputFieldTarga;
+private JTextField inputFieldLunghezza;
+private JPanel pnlLunghezza;
+private JPanel pnlRiempimentoTraghetto;
+private JPanel pnlStoriaInserimenti;
+private JLabel lblUltimoInserito;
+private JButton btnEseguiViaggio;
+private JTextArea txtAreaInseriti;
+private JPanel pnlTasti;
+private JLabel lblEseguite;
+private boolean semaforo;
 
-	//costruisco il traghetto con dimensione massima a piacere(200)
-    private Traghetto <Auto> traghMain =new Traghetto<>(200);
+//costruisco il traghetto con dimensione massima a piacere(200)
+private Traghetto <Auto> traghAttuale =new Traghetto<>(200);
 
-	public Finestra(String title, int larghezza, int altezza) throws HeadlessException {
-		super(title);
-		this.setContentPane(new Finestra().rootPanel);
+public Finestra(String title, int larghezza, int altezza) throws HeadlessException {
+	super(title);
+	this.setContentPane(new Finestra().rootPanel);
 
-		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		this.pack();
-		this.setSize(larghezza, altezza);
-		this.setPosizioneCentro();
-		this.setVisible(true);
-		this.setResizable(false);
+	this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	this.pack();
+	this.setSize(larghezza, altezza);
+	this.setPosizioneCentro();
+	this.setVisible(true);
+	this.setResizable(false);
 
-	}
-
-
-	public Finestra() {
-
-        btnCaricaAuto.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //aggiunge auto con dati da field(parsati) al traghMain
-                Auto toInsert=new Auto((String)inputFieldTarga.getText(),Integer.parseInt(inputFieldLunghezza.getText()));
-
-                if (!traghMain.isTraghettoPieno()){
-
-	                traghMain.addAuto(toInsert);
-	                txtAreaInseriti.append("Auto: "+toInsert.getTarga()+" lunga "+toInsert.getLunghezza()+" caricata");
-	                txtAreaInseriti.append("Posizione: "+traghMain.getNumeroAuto()+" | spazio rimanente:"+traghMain
-			                .getSpazioRimanente());
-
-	                System.out.println("e = [" + e + "]");
-                } else {
-	                lblUltimoInserito.setText("Il traghetto è già pieno!");
-                }
+}
 
 
-            }
-        });
-    }
+public Finestra() {
 
-    public static void main(String[] args) throws Exception {
+	btnCaricaAuto.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//aggiunge auto con dati da field(parsati) al traghetto
 
-        Finestra f = new Finestra("Thall | By Hopeless13",600,350);
+			//controllo che sta aggiungendo qualcosa di sensato
+			if (inputFieldLunghezza.getText().matches("^[-+]?\\d+(\\.\\d+)?$")) {
 
-    }
+				//creo auto da aggiungere alla pila
+				Auto toInsert;
+
+				String targaTMP=inputFieldTarga.getText();
+				int lungezzaTMP=Integer.parseInt(inputFieldLunghezza.getText());
+
+				toInsert = new Auto(targaTMP,lungezzaTMP);
+
+				if (!traghAttuale.isTraghettoPieno()){
+
+					//inizializzazione estetica
+					if (txtAreaInseriti.getText().equals("Ancora nulla qui...")){
+						txtAreaInseriti.setText("");
+					}
+					//effettive aggiunte alla pila e azioni nella GUI
+					traghAttuale.addAuto(toInsert);
+					txtAreaInseriti.append("Auto: "+toInsert.getTarga()+" lunga "+toInsert.getLunghezza()+" caricata"+"\n");
+					txtAreaInseriti.append("Totale mezzi: "+ traghAttuale.getNumeroAuto()+" | spazio rimanente:"+ traghAttuale
+							.getSpazioRimanente()+"\n");
+
+					//pulisco campo inserimento
+					inputFieldTarga.setText("");
+					inputFieldLunghezza.setText("");
+
+				} else {
+					lblUltimoInserito.setText("Il traghetto è già pieno!");
+				}
+			} else {
+				lblUltimoInserito.setText("Inserisci solo numeri (interi) nel campo lunghezza!");
+			}
 
 
-	public void setPosizioneCentro() {
-		// valuta le dimensioni della finestra
-		int larg;
-		int alt;
-		alt = this.getHeight();
-		larg = this.getWidth();
-		// serve per la risoluzione dello schermo
-		final Toolkit kit = Toolkit.getDefaultToolkit();
-		final Dimension dimensione = kit.getScreenSize();
-		final int x = dimensione.width / 2 - larg / 2;
-		final int y = dimensione.height / 2 - alt / 2;
-		this.setLocation(x, y);
-	}
+		}
+	});
+}
+
+public static void main(String[] args) throws Exception {
+
+	Finestra f = new Finestra("Thall | By Hopeless13",600,350);
+
+}
+
+
+public void setPosizioneCentro() {
+	// valuta le dimensioni della finestra
+	int larg;
+	int alt;
+	alt = this.getHeight();
+	larg = this.getWidth();
+	// serve per la risoluzione dello schermo
+	final Toolkit kit = Toolkit.getDefaultToolkit();
+	final Dimension dimensione = kit.getScreenSize();
+	final int x = dimensione.width / 2 - larg / 2;
+	final int y = dimensione.height / 2 - alt / 2;
+	this.setLocation(x, y);
+}
 }
 
 
